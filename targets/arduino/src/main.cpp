c6b9e99b80c5744ecf6ae627ea88c736eb5f58f5
Luke Davies <lkdvs3@gmail.com>
2019-04-07 16:37:39 +0100

    Updated clang-format

--- targets/arduino/src/main.cpp
+++ targets/arduino/src/main.cpp
@@ -1,12 +1,10 @@
 #include <Arduino.h>
 
-void setup()
-{
-    Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
 }
 
-void loop()
-{
-    delay(1000);
-    Serial.println("Hello World!");
+void loop() {
+  delay(1000);
+  Serial.println("Hello World!");
 }
