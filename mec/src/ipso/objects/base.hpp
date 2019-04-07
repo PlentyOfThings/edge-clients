c70fd45f4150ab25ed362a87d358c950362816b8
Luke Davies <lkdvs3@gmail.com>
2019-04-07 16:50:19 +0100

    Added vscode config for lib usage

---
+++ mec/src/ipso/objects/base.hpp
@@ -0,0 +1,19 @@
+#ifndef MEC_IPSO_OBJECTS_BASE_HPP_
+#define MEC_IPSO_OBJECTS_BASE_HPP_
+
+#include "bson/bson.h"
+
+namespace mec {
+namespace ipso {
+
+/**
+ * The base class all IPSO objects derive from.
+ */
+class BaseObject {
+public:
+};
+
+} // namespace ipso
+} // namespace mec
+
+#endif
